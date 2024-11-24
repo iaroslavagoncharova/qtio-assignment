5c67c1afdf0e4056fa170cdfbb6dc37080598a41
Iaroslava Goncharova <111697910+iaroslavagoncharova@users.noreply.github.com>
2024-11-24 21:38:16 +0200

    initial commit

---
+++ src/autocomplete-input.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import AutocompleteTextbox from "./helper-components/autocomplete-textbox";
+import AutocompleteSuggestion from "./helper-components/autocomplete-suggestion";
+import apiClient from "./helper-components/api-client";
+
+const AutocompleteInput = () => {
+  const [value, setValue] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
+  const handleInputChange = (event) => {
+    setValue(event.target.value);
+    setClicked(false);
+    // disable suggestions if there is no input
+    if (event.target.value.length === 0) {
+      setSuggestions([]);
+      setLoading(false);
+      setValue("");
+      return;
+    }
+    // fetch suggestions with apiClient.search
+    setLoading(true);
+    apiClient.search(event.target.value).then((response) => {
+      if (response) {
+        setSuggestions(response);
+      } else {
+        setSuggestions([]);
+      }
+      setLoading(false);
+    });
+  };
+  return (
+    <div>
+      <AutocompleteTextbox
+        onChange={(event) => handleInputChange(event)}
+        value={value}
+      />
+      {loading ? (
+        <div className="loading"> Loading... </div>
+      ) : (
+        <div>
+          {suggestions.length > 0 && !clicked ? (
+            suggestions.map((suggestion, index) => (
+              <div className="autocomplete-item" key={index}>
+                <AutocompleteSuggestion
+                  onClick={() => {
+                    setValue(suggestion);
+                    setSuggestions([]);
+                    setClicked(true);
+                  }}
+                >
+                  {suggestion}
+                </AutocompleteSuggestion>
+              </div>
+            ))
+          ) : value.length > 0 && suggestions.length === 0 && !clicked ? (
+            <div className="no-results">No results</div>
+          ) : null}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AutocompleteInput;
